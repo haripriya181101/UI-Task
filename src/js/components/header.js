40bf6ac2c45529feb728e13a0b268909c8488a7a
Haripriyas <haripriyas@opentrends.net>
2025-04-29 23:23:45 +0530

    added  close icon

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,6 +1,20 @@
 const hamburgerBtn = document.getElementById("hamburgerBtn");
+const closeBtn = document.getElementById("closeBtn");
 const mobileMenu = document.getElementById("mobileMenu");
+const header = document.querySelector(".header"); 
 
 hamburgerBtn.addEventListener("click", () => {
-  mobileMenu.classList.toggle("active");
+  mobileMenu.classList.add("active");
+  header.classList.add("menu-open"); 
+
+  hamburgerBtn.style.display = "none";
+  closeBtn.style.display = "inline";
+});
+
+closeBtn.addEventListener("click", () => {
+  mobileMenu.classList.remove("active");
+  header.classList.remove("menu-open");  
+
+  closeBtn.style.display = "none";
+  hamburgerBtn.style.display = "inline";
 });
