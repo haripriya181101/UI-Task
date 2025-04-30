4e7a78e371a5c8cfc357e990c8f463a1a3cda031
Haripriyas <haripriyas@opentrends.net>
2025-04-30 15:46:08 +0530

    header responsiveness is completed

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,11 +1,11 @@
 const hamburgerBtn = document.getElementById("hamburgerBtn");
 const closeBtn = document.getElementById("closeBtn");
 const mobileMenu = document.getElementById("mobileMenu");
-const header = document.querySelector(".header"); 
+const header = document.querySelector(".header");
 
 hamburgerBtn.addEventListener("click", () => {
   mobileMenu.classList.add("active");
-  header.classList.add("menu-open"); 
+  header.classList.add("menu-open");
 
   hamburgerBtn.style.display = "none";
   closeBtn.style.display = "inline";
@@ -13,7 +13,7 @@ hamburgerBtn.addEventListener("click", () => {
 
 closeBtn.addEventListener("click", () => {
   mobileMenu.classList.remove("active");
-  header.classList.remove("menu-open");  
+  header.classList.remove("menu-open");
 
   closeBtn.style.display = "none";
   hamburgerBtn.style.display = "inline";
