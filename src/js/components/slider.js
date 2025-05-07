fb0adb24aafa6f1d5925c02653c38820ba86cc13
Haripriyas <haripriyas@opentrends.net>
2025-05-07 17:42:51 +0530

    desktop banner is completed ,

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -5,15 +5,13 @@ import "swiper/css/pagination";
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
 
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("hloooo");
-  
   new Swiper(".swiper", {
     modules: [Navigation, Pagination, Autoplay],
     loop: true,
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-    },
+    // autoplay: {
+    //   delay: 3000,
+    //   disableOnInteraction: false,
+    // },
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
