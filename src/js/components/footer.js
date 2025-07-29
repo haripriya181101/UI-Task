b748c8051517322ec53d8eb770cef7a54e97e643
Haripriyas <haripriyas@opentrends.net>
2025-07-29 11:56:36 +0530

    fixed the padding issues of brand and products issues

--- src/js/components/footer.js
+++ src/js/components/footer.js
@@ -1,13 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
   const footerTitles = document.querySelectorAll(".footer__title-wrap");
-  const firstSection = document.querySelector(".footer__section");
+  const sections = document.querySelectorAll(".footer__section");
+  const firstSection = sections[0];
+
   if (window.innerWidth <= 768 && firstSection) {
     firstSection.classList.add("active");
   }
-  footerTitles.forEach(title => {
-    title.addEventListener("click", function () {
-      const clickedSection = title.closest(".footer__section");
-      clickedSection.classList.toggle("active");
-    });
+
+  footerTitles.forEach((title) => {
+    const section = title.closest(".footer__section");
+
+    if (section !== firstSection) {
+      title.addEventListener("click", function () {
+        section.classList.toggle("active");
+      });
+    }
   });
 });
